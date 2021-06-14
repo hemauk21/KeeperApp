702edb08922e5b6576b7cd462ae2eb4d6ead2104
Hemalatha Sivanandam <hemauk21@gmail.com>
2021-06-14 12:07:50 +0530

    Adding my new components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Header() {
+  return (
+    <header>
+      <img
+        src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/274/memo_1f4dd.png"
+        alt="Keeper-App Logo"
+      />
+      <h1>Keeper</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Note(){
+return(
+ <div className="note">
+ <h1>Title of the note </h1>
+ <p>Content of the note</p>
+ </div>   
+ 
+    
+   
+);
+}
+export default Note;
